52da2e5f2413007683469427c23e0d55413884cf
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 09:03:42 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,77 @@
-# require modules here
+'''Ruby
 
 def load_library
-  # code goes here
+   {load_library("./lib/emoticons.yml") }
 end
 
 def get_japanese_emoticon
-  # code goes here
+  # Japanese.emoticon.yml
+  - "O:)"
+  - "☜(⌒▽⌒)☞"
+
+  - ">:("
+  - "ヽ(ｏ`皿′ｏ)ﾉ"
+
+  - ":O"
+  - "(ΘεΘ;)"
+
+  - "%)"
+  - "(゜.゜)"
+
+  - ":$"
+  - "(#^.^#)"
+
+  - "><>"
+  - ">゜))))彡"
+
+  - "8D"
+  - "(^0_0^)"
+
+  - "=D"
+  - "(￣ー￣)"
+
+  - ":)"
+  - "(＾ｖ＾)"
+
+  - ":*"
+  - "(*^3^)/~☆"
+
+  - ":'("
+  - "(Ｔ▽Ｔ)"
+
+  - ":o"
+  - "o_O"
+
+  - ";)"
+  - "(^_-)"
 end
 
 def get_english_meaning
-  # code goes here
+  # English.meaning.yml
+  - angel:
+
+  - angry:
+
+  - bored:
+
+  - confused:
+
+  - embarrased:
+
+  - fish:
+
+  - glasses:
+
+  - grinning
+
+  - happy:
+
+  - kiss:
+
+  - sad:
+
+  - surprised:
+
+  - wink:
+
 end
